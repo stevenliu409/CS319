ed4b44a2bfd5612dbf21de271daf8c1e076cd7b5
AnteZheng <antewater123@hotmail.com>
2013-02-25 00:06:22 -0800

    Made the Admin Layout and edited css to make it look like our design.

---
+++ OFRPDMS/OFRPDMS/Controllers/StaffsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OFRPDMS.Controllers
+{
+    public class StaffsController : Controller
+    {
+        //
+        // GET: /Staffs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
