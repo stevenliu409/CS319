9fa8084f489e9365cc0782a8d2b81f056a6f3963
Peter <answjddnr@gmail.com>
2013-03-04 01:13:15 -0800

    added validation field for Primary Guardian form

---
+++ OFRPDMS/OFRPDMS/Scripts/Custom/PrimaryGuardian/FrontEndValidation.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $(document).ready(function () {
+        $.getScript("../Scripts/Custom/jquery.validate.js", function () {
+        });
+    });
+
+    $("#submit_btn").click(function (e) {
+        $("#PrimaryGuardians").validate();
+
+    });
+
+
+
+});
