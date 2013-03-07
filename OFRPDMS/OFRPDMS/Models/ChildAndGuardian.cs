6e816671b24e34ef56164371076230a6367919c1
Peter <answjddnr@gmail.com>
2013-03-07 14:59:34 -0800

    Peter's commit

---
+++ OFRPDMS/OFRPDMS/Models/ChildAndGuardian.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OFRPDMS.Models
+{
+    public class ChildAndGuardian
+    {
+        public PrimaryGuardian PrimaryGuardian{get;set;}
+        public Child Child { get; set; }
+    }
+}
