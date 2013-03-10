c8a6347a1a9e511708cc59e8aa0e5cc17c74656c
ante zheng <antewater123@hotmail.com>
2013-03-09 22:54:58 -0800

    can now dynamically generate navigation links for different centers when viewed as admin.

--- OFRPDMS/OFRPDMS/Areas/Admin/Controllers/AdminController.cs
+++ OFRPDMS/OFRPDMS/Areas/Admin/Controllers/AdminController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OFRPDMS.Models;
 
 namespace OFRPDMS.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
+
+        private OFRPDMSContext context = new OFRPDMSContext();
         //
         // GET: /Admin/Admin/
 
@@ -15,6 +18,10 @@ namespace OFRPDMS.Areas.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Nav()
+        {
 
+            return PartialView("_AdminNavPartial", context.Centers);
+        }
     }
 }
--- OFRPDMS/OFRPDMS/Controllers/HomeController.cs
+++ OFRPDMS/OFRPDMS/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OFRPDMS.Models;
 
 namespace OFRPDMS.Controllers
 {
     public class HomeController : Controller
     {
+        private OFRPDMSContext context = new OFRPDMSContext();
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -18,6 +20,12 @@ namespace OFRPDMS.Controllers
         public ActionResult About()
         {
             return View();
+        }
+
+        public ActionResult Nav()
+        {
+
+            return PartialView("_AdminNavPartial", context.Centers);
         }
     }
 }
