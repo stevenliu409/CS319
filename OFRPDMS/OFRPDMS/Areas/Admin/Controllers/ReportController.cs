c2f2268c4ff394e5796753e8fc73e1805106173f
rufuszhu <rufus-zhu@hotmail.com>
2013-03-13 16:59:01 -0700

    rufus's commit

--- OFRPDMS/OFRPDMS/Areas/Admin/Controllers/ReportController.cs
+++ OFRPDMS/OFRPDMS/Areas/Admin/Controllers/ReportController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OFRPDMS.Models;
+using OFRPDMS.Areas.Admin.Models;
 
 namespace OFRPDMS.Areas.Admin.Controllers
 {
     public class ReportController : Controller
     {
         OFRPDMSContext context = new OFRPDMSContext();
+        Report myReport = new Report();
+        int[,] pgTable = new int[2,0];
         //
         // GET: /Report/
 
@@ -18,5 +21,43 @@ namespace OFRPDMS.Areas.Admin.Controllers
             return View();
         }
 
+        //
+        // GET: /Report/Generate
+
+        public ActionResult Generate()
+        {
+            ViewBag.numOfNewPG = getNumOfNewPG();
+            return View();
+        }
+
+
+        
+        //
+        // Post: /Index
+
+        [HttpPost]
+        public ActionResult Index(Report report)
+        {
+            myReport = report;
+            return RedirectToAction("Generate","Report");
+        }
+
+        // get the number of new PG(created after start date) 
+        private int getNumOfNewPG()
+        {
+            return (from u in context.PrimaryGuardians
+                    where u.DateCreated > myReport.startDay
+                    select u).ToArray().Length;
+        }
+
+        private int[,] createPGTable()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+
+            }
+                return pgTable;
+        }
+        
     }
 }
---
+++ OFRPDMS/OFRPDMS/Areas/Admin/Models/Report.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OFRPDMS.Areas.Admin.Models
+{
+    public class Report
+    {
+        public int[] CenterList { get; set; }
+        public DateTime startDay { get; set; }
+        public DateTime endDay { get; set; }
+    }
+}
