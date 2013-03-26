81f6c9fbd6f68d3d8e35329c3f9bd9ff1342fa35
rufuszhu <rufus-zhu@hotmail.com>
2013-03-25 17:00:32 -0700

    adding trackPG

--- OFRPDMS/OFRPDMS/Areas/Admin/Controllers/ReportController.cs
+++ OFRPDMS/OFRPDMS/Areas/Admin/Controllers/ReportController.cs
@@ -105,22 +105,22 @@ namespace OFRPDMS.Areas.Admin.Controllers
 
 
             // create a new excel workbook
-            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
-            Microsoft.Office.Interop.Excel.Workbook workBook = excel.Workbooks.Add();
-            Microsoft.Office.Interop.Excel.Worksheet sheet = workBook.ActiveSheet;
+            //Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            //Microsoft.Office.Interop.Excel.Workbook workBook = excel.Workbooks.Add();
+            //Microsoft.Office.Interop.Excel.Worksheet sheet = workBook.ActiveSheet;
 
             // File in the excel cells with report data here if we are in an excel mode
-            if (mode == 1 || mode == 2)
-            {
-                for (int i = 0; i < Content.Length; i++)
-                {
-                    string[] CsvLine = Content[i];
-                    for (int j = 0; j < CsvLine.Length; j++)
-                    {
-                        sheet.Cells[i + 1, j + 1] = CsvLine[j];
-                    }
-                }
-            }
+            //if (mode == 1 || mode == 2)
+            //{
+            //    for (int i = 0; i < Content.Length; i++)
+            //    {
+            //        string[] CsvLine = Content[i];
+            //        for (int j = 0; j < CsvLine.Length; j++)
+            //        {
+            //            sheet.Cells[i + 1, j + 1] = CsvLine[j];
+            //        }
+            //    }
+            //}
 
             // filename for temporary excel file
             string tempFileName = Path.GetTempFileName();
@@ -128,36 +128,36 @@ namespace OFRPDMS.Areas.Admin.Controllers
 
             if (mode == 1)
             {
-                // .xls file format, 2003 and older
-                workBook.SaveAs(tempFileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlAddIn8);
-                workBook.Close();
+                //// .xls file format, 2003 and older
+                //workBook.SaveAs(tempFileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlAddIn8);
+                //workBook.Close();
 
-                Response.ContentType = "application/vnd.ms-excel";
-                Response.AddHeader("content-disposition", "attachment; filename=Report.xls");
+                //Response.ContentType = "application/vnd.ms-excel";
+                //Response.AddHeader("content-disposition", "attachment; filename=Report.xls");
 
-                Response.WriteFile(tempFileName);
-                Response.Flush();
+                //Response.WriteFile(tempFileName);
+                //Response.Flush();
 
-                // clean temp file
-                System.IO.File.Delete(tempFileName);
-                Response.End();
+                //// clean temp file
+                //System.IO.File.Delete(tempFileName);
+                //Response.End();
             }
             else if (mode == 2)
             {
-                // .xlsx file format, 2007 and newer
-                // maybe try to find the corresponding XlFileFormat so we don't rely on the Interop version to choose
-                workBook.SaveAs(tempFileName);
-                workBook.Close();
+                //// .xlsx file format, 2007 and newer
+                //// maybe try to find the corresponding XlFileFormat so we don't rely on the Interop version to choose
+                //workBook.SaveAs(tempFileName);
+                //workBook.Close();
 
-                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-                Response.AddHeader("content-disposition", "attachment; filename=Report.xlsx");
+                //Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                //Response.AddHeader("content-disposition", "attachment; filename=Report.xlsx");
 
-                Response.WriteFile(tempFileName);
-                Response.Flush();
+                //Response.WriteFile(tempFileName);
+                //Response.Flush();
 
-                // clean temp file
-                System.IO.File.Delete(tempFileName);
-                Response.End();
+                //// clean temp file
+                //System.IO.File.Delete(tempFileName);
+                //Response.End();
             }
             else if (mode == 3)
             {
