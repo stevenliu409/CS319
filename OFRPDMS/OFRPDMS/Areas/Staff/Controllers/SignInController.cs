53cb303b4c6be47f2fd2a6a538a5bbfe636bcc7c
Steven <steven.github@gmail.com>
2013-03-27 11:34:47 -0700

    fixed center deletion and removed child birthday from search bar

--- OFRPDMS/OFRPDMS/Areas/Staff/Controllers/SignInController.cs
+++ OFRPDMS/OFRPDMS/Areas/Staff/Controllers/SignInController.cs
@@ -152,7 +152,7 @@ namespace OFRPDMS.Areas.Staff.Controllers
                     id = pm.Id,
                     Fname = pm.FirstName,
                     Lname = pm.LastName,
-                    birthday = pm.Birthdate.Value.ToString("MM/dd/yyyy"),
+                    //birthday = pm.Birthdate.Value.ToString("MM/dd/yyyy"),
                     relationshiptoGuardian = pm.PrimaryGuardian.FirstName,
                     allergy = pm.Allergies,
                     type = 3
--- OFRPDMS/OFRPDMS/Models/Model1.edmx.sql
+++ OFRPDMS/OFRPDMS/Models/Model1.edmx.sql
@@ -538,7 +538,7 @@ ADD CONSTRAINT [FK_CenterEvent]
     FOREIGN KEY ([CenterId])
     REFERENCES [dbo].[Centers]
         ([Id])
-    ON DELETE CASCADE ON UPDATE NO ACTION;
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_CenterEvent'
 CREATE INDEX [IX_FK_CenterEvent]
@@ -720,7 +720,7 @@ ADD CONSTRAINT [FK_EventEventParticipant]
     FOREIGN KEY ([EventId])
     REFERENCES [dbo].[Events]
         ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+    ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_EventEventParticipant'
 CREATE INDEX [IX_FK_EventEventParticipant]
