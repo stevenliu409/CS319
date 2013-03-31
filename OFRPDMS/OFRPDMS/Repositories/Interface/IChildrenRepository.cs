441ba1a8dffc7a11686c0a86292b273023232f32
kev248 <kev248@hotmail.com>
2013-03-31 14:37:19 -0700

    Add repo files

---
+++ OFRPDMS/OFRPDMS/Repositories/Interface/IChildrenRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OFRPDMS.Models;
+
+namespace OFRPDMS.Repositories
+{
+    public interface IChildRepository
+    {
+        Child FindById(int id);
+
+        void Add(Child c);
+
+        void Update(Child c);
+
+        void Delete(Child c);
+
+        void Dispose();
+    }
+}
---
+++ OFRPDMS/OFRPDMS/Repositories/Interface/IPrimaryGuardianRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OFRPDMS.Models;
+
+namespace OFRPDMS.Repositories
+{
+    public interface IPrimaryGuardianRepository
+    {
+        PrimaryGuardian FindById(int id);
+
+        IEnumerable<PrimaryGuardian> FindAllWithCenterId(int centerId);
+
+        PrimaryGuardian FindByIdAndCenterId(int id, int centerId);
+
+        void Add(PrimaryGuardian p);
+
+        void Update(PrimaryGuardian p);
+
+        void Delete(PrimaryGuardian p);
+
+        void Dispose();
+    }
+}
